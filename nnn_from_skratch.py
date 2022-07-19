c0d42faf67f19801bc2a38587e6cf439097a5cd8
valteu <62844981+valteu@users.noreply.github.com>
2022-07-20 01:01:00 +0200

    added activation function

--- nnn_from_skratch.py
+++ nnn_from_skratch.py
@@ -1,11 +1,46 @@
+import sys
 import numpy as np
+import matplotlib
 
-inputs = [1, 2, 3, 2.5]
-weights = [[0.2, 0.8, -0.5, 1.0],
-		   [0.5, -0.91, 0.26, -0.5],
-		   [-0.26, -0.27, 0.17, 0.87]]
+X = [[1.2, 5.1, 2.1, 3.2],
+	 [2.0, 5.0, -1.0, 2.0],
+	 [-1.5, 2.7, 3.3, -0.8]]
 
-biases = [2, 3, 0.5]
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
 
-output = np.dot(weights, inputs) + biases
+#https://cs231n.github.io/neural-networks-case-study/
+def spiral_data(points, classes):
+    X = np.zeros((points*classes, 2))
+    y = np.zeros(points*classes, dtype='uint8')
+    for class_number in range(classes):
+        ix = range(points*class_number, points*(class_number+1))
+        r = np.linspace(0.0, 1, points)  # radius
+        t = np.linspace(class_number*4, (class_number+1)*4, points) + np.random.randn(points)*0.2
+        X[ix] = np.c_[r*np.sin(t*2.5), r*np.cos(t*2.5)]
+        y[ix] = class_number
+    return X, y
+
+X, y = spiral_data(100, 3)
 print(output)
+
+class Layer_Dense:
+
+	def __init__(self, n_inputs, n_neurons):
+		self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+		self.biases = np.zeros((1, n_neurons))
+
+	def forward(self, inputs):
+		self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+
+	def forward(self, inputs):
+		self.output = np.maximum(0, inputs)
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
+
+layer1.forward(X)
+activation1.forward(layer1.output)
+print(activation1.output)
