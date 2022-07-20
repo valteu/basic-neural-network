252acfd2ce532f7aecb3c8f93ce8d7bb8107e1ad
valteu <62844981+valteu@users.noreply.github.com>
2022-07-20 13:38:36 +0200

    added softmax activation for output neurons

--- nnn_from_skratch.py
+++ nnn_from_skratch.py
@@ -2,12 +2,6 @@ import sys
 import numpy as np
 import matplotlib
 
-X = [[1.2, 5.1, 2.1, 3.2],
-	 [2.0, 5.0, -1.0, 2.0],
-	 [-1.5, 2.7, 3.3, -0.8]]
-
-inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
-output = []
 
 #https://cs231n.github.io/neural-networks-case-study/
 def spiral_data(points, classes):
@@ -21,26 +15,34 @@ def spiral_data(points, classes):
         y[ix] = class_number
     return X, y
 
-X, y = spiral_data(100, 3)
-print(output)
-
 class Layer_Dense:
-
 	def __init__(self, n_inputs, n_neurons):
 		self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
 		self.biases = np.zeros((1, n_neurons))
-
 	def forward(self, inputs):
 		self.output = np.dot(inputs, self.weights) + self.biases
 
 class Activation_ReLU:
-
 	def forward(self, inputs):
 		self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(2, 5)
+class Activation_Softmax:
+	def forward(self, inputs):
+		exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+		probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+		self.output = probabilities
+
+X, y = spiral_data(100, 3)
+
+dense1 =Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+print(activation2.output)
